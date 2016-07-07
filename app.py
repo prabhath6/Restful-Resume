7a5749ff313cb1ad9c4b864ffdd270f737b6ecb4
Prabhath kiran <prabhathkiran20@gmail.com>
2016-07-07 08:53:04 -0700

    add complete resume endpoint

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
 
-print(os.environ["USERNAME"])
-print(os.environ["PASSWORD"])
+
+app.config['MONGO_DBNAME'] = "resume" # adding database name
+app.config["MONGO_URI"] = "mongodb://"+ os.environ["USERNAME"] + ":" + os.environ["PASSWORD"] + "@ds015995.mlab.com:15995/resume"
+
+# print(os.environ["USERNAME"])
+# print(os.environ["PASSWORD"])
+
+mongo = PyMongo(app)
 
 @app.route("/", methods=["GET"])
 def home():
     return "test"
 
+@app.route("/resume", methods=["GET"])
+def complete_resume():
+    frame_work = mongo.db.my_resume
+
+    output = []
+    for i in frame_work.find():
+        output.append({"contact" : i["contact_info"], "edu" : i["Education"], "Exp": i["Experience"],
+                       "skills" : i["Skills"], "projects" : i["Projects"], "links" : i["Links"]})
+
+    return jsonify({"result" : output})
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
+app = Flask(__name__)
